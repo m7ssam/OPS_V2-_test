046d636cd3d1358b6ae146015892fcdee15fcccd
m7ossam <mohamed7ossam@outlook.com>
2023-11-08 21:33:04 +0200

    fix login page

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import RegisterForm
-# Create your views here.
+from django.contrib.auth import login, logout, authenticate
 
 def login(request):
   return render(request, 'users\login.html')
@@ -9,6 +9,10 @@ def login(request):
 def sign_up(request):
   if request.method == "POST":
     form = RegisterForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect ('')
   else:
     form = RegisterForm()
   return render(request, "registration/sign_up.html", {"form": form})
